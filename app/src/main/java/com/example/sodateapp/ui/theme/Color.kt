d311361a5cee028cb1ebf56fcc86406f487b89bd
G-hidekazu <ghidekazutanaka@gmail.com>
2026-01-18 22:53:18 +0900

    Add simple plant watering Android app

---
+++ app/src/main/java/com/example/sodateapp/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.sodateapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenLight = Color(0xFF8AD08C)
+val GreenDark = Color(0xFF4A8B57)
+val OrangeLight = Color(0xFFFFD8B5)
+val OrangeDark = Color(0xFFE9A971)
---
+++ app/src/main/java/com/example/sodateapp/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.example.sodateapp.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography()
